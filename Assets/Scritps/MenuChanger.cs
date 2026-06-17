d7cd584f0d34608cd7f9e4b012c4b120c7e81e7d
cogdi <hotpursit0603@gmail.com>
2026-06-18 01:43:04 +0500

    feat: new menus, transitions between them.

---
+++ Assets/Scritps/MenuChanger.cs
@@ -0,0 +1,34 @@
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class MenuChanger : MonoBehaviour
+// {
+//     // [SerializeField] private Dictionary<string, GameObject> PanelsDictionary
+//     // {
+        
+//     // }
+//     [SerializeField] private List<GameObject> panels;
+
+
+//     // public enum PanelNames
+//     // {
+//     //     Licenses,
+//     //     Exercises,
+//     //     News
+//     // }
+
+//     private void Start()
+//     {
+//         for (int i = 1; i < panels.Count; i++)
+//         {
+//             panels[i].SetActive(false);
+//         }
+        
+//         panels[0].SetActive(true);
+//     }
+
+//     private void Update()
+//     {
+        
+//     }
+// }
