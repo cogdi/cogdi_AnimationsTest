0d5c790403e2d1cb1e442253a461e0699b550441
cogdi <hotpursit0603@gmail.com>
2026-06-21 12:20:08 +0500

    fix: Cross at the window's corner now closes the window

--- Assets/Scritps/DialogBox.cs
+++ Assets/Scritps/DialogBox.cs
@@ -5,11 +5,13 @@ public class DialogBox : MonoBehaviour
 {
     [SerializeField] private Button yesButton;
     [SerializeField] private Button noButton;
+    [SerializeField] private Button closeButton;
 
     private void Awake()
     {
         yesButton.onClick.AddListener(CloseDialogBox);
         noButton.onClick.AddListener(CloseDialogBox);
+        closeButton.onClick.AddListener(CloseDialogBox);
     }
 
     private void CloseDialogBox()
